260bd6ea2767a1b517b3406f6e67c8328b9d8700
Vladimir Bazhanov <vladimir.bazhanov@flatstack.com>
2017-08-11 21:34:50 +0300

    Removed unused fields

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170811141450) do
+ActiveRecord::Schema.define(version: 20170811183145) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,11 +55,9 @@ ActiveRecord::Schema.define(version: 20170811141450) do
   create_table "topics", force: :cascade do |t|
     t.integer "course_id"
     t.string "title"
-    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "order_index"
-    t.text "content"
   end
 
 end
