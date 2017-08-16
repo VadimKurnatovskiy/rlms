572bf504d3a0cb13dfb1d31663d978e40ae33746
Vladimir Bazhanov <vladimir.bazhanov@flatstack.com>
2017-08-16 19:31:48 +0300

    Work with models

--- spec/models/course_participation_spec.rb
+++ spec/models/course_participation_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 describe CourseParticipation do
+  it { is_expected.to validate_presence_of :course_id }
+  it { is_expected.to validate_presence_of :learner_id }
+
+  it { is_expected.to validate_uniqueness_of(:course_id).scoped_to(:learner_id) }
+
   it { is_expected.to belong_to :course }
   it { is_expected.to belong_to :learner }
 end
