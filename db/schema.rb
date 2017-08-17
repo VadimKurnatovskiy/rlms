e0e708d8bbc85d379e3430ec913a8b995a2a920a
Vladimir Bazhanov <vladimir.bazhanov@flatstack.com>
2017-08-17 18:57:02 +0300

    replaced link with title

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170817140839) do
+ActiveRecord::Schema.define(version: 20170817154415) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,10 +61,11 @@ ActiveRecord::Schema.define(version: 20170817140839) do
 
   create_table "materials", force: :cascade do |t|
     t.integer "topic_id"
-    t.string "link"
     t.string "attachment"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
+    t.integer "order_index"
   end
 
   create_table "teachers", force: :cascade do |t|
