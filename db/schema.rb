4f2909d93849274d43283265d3287badc0df163e
Vladimir Bazhanov <vladimir.bazhanov@flatstack.com>
2017-08-18 15:21:56 +0300

    Added Question model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170817154415) do
+ActiveRecord::Schema.define(version: 20170818120910) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,13 @@ ActiveRecord::Schema.define(version: 20170817154415) do
     t.datetime "updated_at", null: false
     t.string "title"
     t.integer "order_index"
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.integer "topic_id"
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "teachers", force: :cascade do |t|
--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -3,4 +3,7 @@ require 'rails_helper'
 describe Topic do
   it { is_expected.to validate_presence_of :title }
   it { is_expected.to validate_presence_of :course_id }
+
+  it { is_expected.to have_many(:materials) }
+  it { is_expected.to have_many(:questions) }
 end
