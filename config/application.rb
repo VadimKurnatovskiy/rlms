e1ecfad2fec481e1c97d222144748d4efc08dfc1
Vladimir Bazhanov <vladimir.bazhanov@flatstack.com>
2017-09-05 21:44:30 +0300

    RU locale

--- config/application.rb
+++ config/application.rb
@@ -18,14 +18,11 @@ Bundler.require(*Rails.groups)
 
 module Rlms
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    I18n.load_path += Dir[Rails.root.join("config", "locales", "**/*.{rb,yml}").to_s]
+    config.i18n.default_locale = :ru
 
-    # Don't generate system test files.
     config.generators.system_tests = nil
   end
 end
