420ed67839be24ee63e56f613282b49ec974754e
Vladimir Bazhanov <vladimir.bazhanov@flatstack.com>
2017-09-07 21:15:57 +0300

    Reverted locale to :en

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ module Rlms
     config.load_defaults 5.1
 
     I18n.load_path += Dir[Rails.root.join("config", "locales", "**/*.{rb,yml}").to_s]
-    config.i18n.default_locale = :ru
+    config.i18n.default_locale = :en
 
     config.generators.system_tests = nil
   end
