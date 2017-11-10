afed06d52065a52b459ea079d08797bf72a38832
Viktoriya <viisqas@gmail.com>
2017-11-10 11:38:03 +0300

    add info in dashboard

--- app/controllers/teachers/dashboard_controller.rb
+++ app/controllers/teachers/dashboard_controller.rb
@@ -1,5 +1,16 @@
 module Teachers
   class DashboardController < BaseController
-    def show; end
+    respond_to :html
+
+    expose :course, find_by: :slug, id: :slug
+    expose :courses, from: :current_teacher
+
+    def show 
+      @total_students = 0
+      current_teacher.courses.each do |course|
+        @total_students += course.learners.count
+      end
+     respond_with courses, @total_students
+    end
   end
 end
