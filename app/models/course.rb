3f59e6eb9c2c90038ab1211fd0707a7653a0c777
programmemann <pavlov_egor007@mail.ru>
2017-12-19 12:54:51 +0300

    add validate with whitelist

--- app/models/course.rb
+++ app/models/course.rb
@@ -6,6 +6,7 @@ class Course < ApplicationRecord
   has_many :course_participations
   has_many :learners, through: :course_participations
 
+  validate  :slug_course_validate
   validates :title, :slug, :description, :teacher_id, presence: true
   validates :title, :slug, :description, uniqueness: true
 
@@ -13,6 +14,13 @@ class Course < ApplicationRecord
   scope :not_published, -> { where(published: false) }
 
   scope :ordered_by_title, -> { order(title: :asc) }
+
+  WHITELIST = /^[a-zA-Z0-9_-]*$/
+  def slug_course_validate
+    unless WHITELIST=~ slug 
+      errors[:slug] << "Slug not valid" 
+    end
+  end
 
   def to_param
     slug
